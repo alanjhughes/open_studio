a2eab6b7eb66d95c93cfabe3478954d049ad4785
Alan Hughes <hughesno1@gmail.com>
2022-04-03 12:41:37 +0100

    Add anyhow error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate core;
 
+use anyhow::{Context, Result};
 use std::path::PathBuf;
 use std::process::Command;
 
 use clap::Parser;
+
+#[derive(Debug)]
+struct DirError(String);
 
 #[derive(Parser, Debug)]
 #[clap(name = "studio")]
@@ -12,13 +16,16 @@ struct Args {
     dir: PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let args: Args = Args::parse();
 
     let mut command = Command::new("open");
     command.arg("-a").arg("Android Studio.app").arg("--args");
 
-    let path = args.dir.canonicalize()?;
+    let path = args
+        .dir
+        .canonicalize()
+        .with_context(|| format!("Error: could not find directory `{}`", args.dir.display()))?;
 
     command.arg(path);
 
